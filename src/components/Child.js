dbdef8732a11ac5968688871f7ef75483a56dba4
Polash Biswas <polashbiswasit19@gmail.com>
2023-06-15 19:59:21 +0530

    submitting code-Thu Jun 15 2023 19:59:12 GMT+0530 (India Standard Time)

---
+++ src/components/Child.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+const Child = ({ showlist, setlist }) => {
+
+    function handelRemove(index){
+        const newData = [...showlist];
+        newData.splice(index, 1);
+        setlist(newData);
+    }
+
+
+    return (
+        <div className="child">
+            <h2>Child Component</h2>
+            <ul>
+                {
+                    showlist.map((data, index) => (
+                        <li key={index}>{data}
+                        <button onClick={()=>handelRemove(index)}>Remove</button>
+                        </li>
+                        
+                    ))
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default Child;
---
+++ src/components/Parent.js
@@ -0,0 +1,49 @@
+import React,{useState} from "react";
+import Child from "./Child";
+
+
+const Parent = () => {
+
+    let [cartItems, setCartItems] = useState("");
+    let [cartPrice, setCartPrice] = useState("");
+    let [showData, setShowData] = useState([]);
+
+    function handelForm(e){
+        e.preventDefault();
+
+        setShowData([...showData,`${cartItems} - $${cartPrice}`]);
+       setCartItems("");
+       setCartPrice("");
+    }
+    // console.log(arr);
+    // console.log(showData);
+
+
+    return(
+        <div className="parent">
+            <h1>Parent Component</h1>
+            <form>
+            <label htmlFor="itemName">Item Name</label>
+            <input 
+            type="text" 
+            id="itemName" 
+            onChange={(e)=>setCartItems(e.target.value)}
+            value={cartItems}
+            />
+
+            <label htmlFor="itemPrice">Item Price</label>
+            <input type="number"
+             id="itemPrice" 
+             onChange={(e)=>setCartPrice(e.target.value)}
+             value={cartPrice}
+             />
+
+            <button onClick={handelForm}>Add Item</button>
+            </form>
+
+            <Child showlist={showData} setlist={setShowData}/>
+        </div>
+    )
+}
+
+export default Parent;
